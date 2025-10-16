2f8f42f5aa3e8ead0034c6fd9c08a494ca38fa09
RainPPR <PPR2125773894@163.com>
2025-10-16 20:41:15 +0800

    Update export.js

--- material/templates/assets/javascripts/export.js
+++ material/templates/assets/javascripts/export.js
@@ -1,4 +1,4 @@
 document$.subscribe(({ body }) => {
     const exportParam = new URLSearchParams(location.search).get('export')?.toLowerCase();
-    exportParam === 'true' ? do_not_export() : do_export();
+    exportParam === 'true' ? do_export() : do_not_export();
 });
